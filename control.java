5cbe8f0b6d911f2b3b3ad5a7f8de17dc952857d3
VENKI-13 <venkatesh13032005@gmail.com>
2026-04-29 14:16:23 +0530

    Commit Signed-off-by: VENKI-13 <venkatesh13032005@gmail.com>

--- control.java
+++ control.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
 class control{
     public static void main(String[] args){
-        int m = 2016;
-        if(m%400==0 || m%4==0 && m%100==0)
+        Scanner s = new Scanner(System.in);
+        System.out.print("Year: ");
+        int m = s.nextInt();
+        if(m%400==0 || m%4==0 && m%100!=0)
             System.out.println("Leap year");
         else
             System.out.println("Not Leap year");
---
+++ increment.java
@@ -0,0 +1,16 @@
+public class increment{
+    public static void main(String[] args) {
+        int a =  5, b = 5;
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a);
+        a  = 8; b = 15;
+        System.out.println((a==b)?"Yes":"No");
+        System.out.println(a&b);
+        System.out.println(a|b);
+        System.out.println(a^b);
+        System.out.println(~a);
+        System.out.println(a<<1);
+        System.out.println(1>>b);
+    }
+}
