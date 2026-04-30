f9abca78f8694e7123c2ca2000e83049eb190978
VENKI-13 <venkatesh13032005@gmail.com>
2026-04-30 08:31:35 +0530

    Next Commit Signed-off-by: VENKI-13 <venkatesh13032005@gmail.com>

---
+++ grade.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class grade {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int marks = sc.nextInt();
+        
+        if (marks >= 40) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
