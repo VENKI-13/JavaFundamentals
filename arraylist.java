6f3573ad4997447dd0d54665cec09746aad44db2
VENKI-13 <venkatesh13032005@gmail.com>
2026-05-02 11:42:04 +0530

    Commit Signed-off-by: VENKI-13 <venkatesh13032005@gmail.com>

--- arraylist.java
+++ arraylist.java
@@ -4,7 +4,10 @@ class arraylist {
     public static void main(String[] args) {
         ArrayList<Integer> arr = new ArrayList<>();
         arr.add(0);
-        arr.addAll(Arrays.asList(1,2,3,4));
+        arr.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+        System.out.println(arr);
+        arr.remove(9);
+        arr.set(8,13);
         System.out.println(arr);
     }
 }
